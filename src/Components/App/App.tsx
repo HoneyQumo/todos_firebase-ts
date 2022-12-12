08e02313f3022b5c4a2a3146ff6e9aab3457287d
HoneyQumo <HoneyQumo@gmail.com>
2022-12-12 16:55:29 +0300

    v0.0.11 | Logout.tsx FIXING.

--- src/Components/App/App.tsx
+++ src/Components/App/App.tsx
@@ -1,4 +1,4 @@
-import React, {memo, useLayoutEffect, useState} from 'react'
+import React, {useEffect, useState} from 'react'
 import {BrowserRouter as Router, NavLink, Route, Routes} from 'react-router-dom'
 import {del, getList, setDone} from '../../auth'
 import {getAuth, User, onAuthStateChanged} from 'firebase/auth'
@@ -13,114 +13,116 @@ import Logout from '../Logout/Logout'
 
 
 const App: React.FC = () => {
-  const [data, setData] = useState<IDeed[]>([])
-  const [showMenu, setShowMenu] = useState<boolean>(false)
-  const [currentUser, setCurrentUser] = useState<User | null>(null)
+	const [data, setData] = useState<IDeed[]>([])
+	const [showMenu, setShowMenu] = useState<boolean>(false)
+	const [currentUser, setCurrentUser] = useState<User | null>(null)
 
-  useLayoutEffect(() => {
-    onAuthStateChanged(getAuth(firebaseApp), (user) => {
-      if (user) {
-        authStateChanged(user)
-      }
-    })
-  }, [])
+	//TODO: при выходе не работает Logout.
+	async function authStateChanged(user: User) {
+		if (user) {
+			setCurrentUser(user)
+			const newData = await getList(user)
+			setData(newData)
+		} else {
+			setCurrentUser(null)
+			setData([])
+		}
+	}
 
-  async function authStateChanged(user: User) {
-    setCurrentUser(user)
-    if (user) {
-      const newData = await getList(user)
-      setData(newData)
-    } else {
-      setData([])
-    }
-  }
+	useEffect(() => {
+		onAuthStateChanged(getAuth(firebaseApp), (user) => {
+			if (user) {
+				authStateChanged(user)
+			}
+		})
+	}, [])
 
-  function getDeed(key: string) {
-    return data.find(deed => deed.key === key)
-  }
+	function getDeed(key: string) {
+		return data.find(deed => deed.key === key)
+	}
 
-  function showMenuToggle(event: React.MouseEvent<HTMLDivElement | HTMLAnchorElement, MouseEvent>) {
-    event.preventDefault()
-    setShowMenu(prevState => !prevState)
-  }
+	function showMenuToggle(event: React.MouseEvent<HTMLDivElement | HTMLAnchorElement, MouseEvent>) {
+		event.preventDefault()
+		setShowMenu(prevState => !prevState)
+	}
 
-  function addDeed(deed: IDeed) {
-    setData(prevState => [...prevState, deed])
-  }
+	function addDeed(deed: IDeed) {
+		setData(prevState => [...prevState, deed])
+	}
 
-  async function deleteDeed(key: string) {
-    if (currentUser) {
-      await del(currentUser, key)
-    }
-    const newData = data.filter((current) => current.key !== key)
-    setData(newData)
-  }
+	async function deleteDeed(key: string) {
+		if (currentUser) {
+			await del(currentUser, key)
+		}
+		const newData = data.filter((current) => current.key !== key)
+		setData(newData)
+	}
 
-  async function setDoneDeed(key: string) {
-    if (currentUser) {
-      await setDone(currentUser, key)
-    }
-    const newData = data.map(deed => deed.key === key
-      ? {...deed, done: !deed.done}
-      : deed,
-    )
-    setData(newData)
-  }
+	async function setDoneDeed(key: string) {
+		if (currentUser) {
+			await setDone(currentUser, key)
+		}
+		const newData = data.map(deed => deed.key === key
+			? {...deed, done: !deed.done}
+			: deed,
+		)
+		setData(newData)
+	}
 
-  return (
-    <Router>
-      <nav className="navbar is-light">
-        <div className="navbar-brand">
-          <NavLink to="/" className={({isActive}) => `navbar-item is-uppercase ${isActive ? 'is-active' : ''}`}>
-            {currentUser ? currentUser.email : 'Todos'}
-          </NavLink>
-          <a href="/" className={showMenu ? 'navbar-burger is-active' : 'navbar-burger'}
-             onClick={showMenuToggle}
-          >
-            <span></span>
-            <span></span>
-            <span></span>
-          </a>
-        </div>
-        <div className={showMenu ? 'navbar-menu is-active' : 'navbar-menu'} onClick={showMenuToggle}>
-          <div className="navbar-start">
-            {currentUser && (
-              <NavLink to="/add" className={({isActive}) => `navbar-item ${isActive ? 'is-active' : ''}`}>
-                Создать дело
-              </NavLink>)}
-            {!currentUser && (
-              <NavLink to="/login" className={({isActive}) => `navbar-item ${isActive ? 'is-active' : ''}`}>
-                Войти
-              </NavLink>
-            )}
-            {!currentUser && (
-              <NavLink to="/register"
-                       className={({isActive}) => `navbar-item ${isActive ? 'is-active' : ''}`}>Зарегистрироваться
-              </NavLink>)}
-            {currentUser && (
-              <div className="navbar-end">
-                <NavLink to="/logout" className={({isActive}) => `navbar-item ${isActive ? 'is-active' : ''}`}>
-                  Выйти
-                </NavLink>
-              </div>
-            )}
-          </div>
-        </div>
-      </nav>
-      <main className="content px-6 mt-6">
-        <Routes>
-          <Route path="/"
-                 element={<TodoList setDoneDeed={setDoneDeed} deleteDeed={deleteDeed} list={data}
-                                    currentUser={currentUser}/>}/>
-          <Route path="/add" element={<TodoAdd addDeed={addDeed} currentUser={currentUser}/>}/>
-          <Route path="/:key" element={<TodoDetail getDeed={getDeed} currentUser={currentUser}/>}/>
-          <Route path="/register" element={<Register currentUser={currentUser}/>}/>
-          <Route path="/login" element={<Login currentUser={currentUser}/>}/>
-          <Route path="/logout" element={<Logout currentUser={currentUser}/>}/>
-        </Routes>
-      </main>
-    </Router>
-  )
+	return (
+		<Router>
+			<nav className="navbar is-light">
+				<div className="navbar-brand">
+					<NavLink to="/" className={({isActive}) => `navbar-item is-uppercase ${isActive ? 'is-active' : ''}`}>
+						{currentUser ? currentUser.email : 'Todos'}
+					</NavLink>
+					<a href="/" className={showMenu ? 'navbar-burger is-active' : 'navbar-burger'}
+						 onClick={showMenuToggle}
+					>
+						<span></span>
+						<span></span>
+						<span></span>
+					</a>
+				</div>
+				<div className={showMenu ? 'navbar-menu is-active' : 'navbar-menu'} onClick={showMenuToggle}>
+					<div className="navbar-start">
+						{currentUser && (
+							<NavLink to="/add" className={({isActive}) => `navbar-item ${isActive ? 'is-active' : ''}`}>
+								Создать дело
+							</NavLink>)}
+						{!currentUser && (
+							<NavLink to="/login" className={({isActive}) => `navbar-item ${isActive ? 'is-active' : ''}`}>
+								Войти
+							</NavLink>
+						)}
+						{!currentUser && (
+							<NavLink to="/register"
+											 className={({isActive}) => `navbar-item ${isActive ? 'is-active' : ''}`}>Зарегистрироваться
+							</NavLink>)}
+						{currentUser && (
+							<div className="navbar-end">
+								<NavLink to="/logout" className={({isActive}) => `navbar-item ${isActive ? 'is-active' : ''}`}>
+									Выйти
+								</NavLink>
+							</div>
+						)}
+					</div>
+				</div>
+			</nav>
+			<main className="content px-6 mt-6">
+				<Routes>
+					<Route path="/"
+								 element={<TodoList setDoneDeed={setDoneDeed} deleteDeed={deleteDeed} list={data}
+																		currentUser={currentUser}/>}/>
+					<Route path="/add" element={<TodoAdd addDeed={addDeed} currentUser={currentUser}/>}/>
+					<Route path="/:key" element={<TodoDetail getDeed={getDeed} currentUser={currentUser}/>}/>
+					<Route path="/register" element={<Register currentUser={currentUser}/>}/>
+					<Route path="/login" element={<Login currentUser={currentUser}/>}/>
+					<Route path="/logout" element={<Logout currentUser={currentUser}/>}/>
+				</Routes>
+			</main>
+		</Router>
+	)
 }
 
 export default App
